61d1908c707a2a336815c86c3bd34f2a852f6283
GabrielaCR <gabriela.calistro@hotmail.com>
2017-08-12 23:52:06 +0200

    New filter settings, ADD_filters option
    Main changes:
    
    - Dictionary ADDfilters[] was added, inside the filters[] dictionary.
    In ADDfilters[] the user can enter the names, files and other
    properties of the filters he/she wants to add. These will be then read
    out in FILTER_AGNfitter.py
    - Function MODELS_settings() (with dictionary models []) was added,
    which will be further implemented soon.
    
    Minor changes:
    
    - cat['dict_path'] was removed. The name of the dictionary of models
    will be now automatically created as a combination of the names of the
    filterset (specified by the user in filters['filterset’]) and the
    modelset (specified by the user in models['modelset’] ). This should
    give less issues with the overwriting of dictionaries.
    -FITS handling was already implemented, but comments in this file were
    not updated till now.

--- example/SETTINGS_AGNfitter.py
+++ example/SETTINGS_AGNfitter.py
@@ -28,17 +28,13 @@ def CATALOG_settings():
 
     ##GENERAL
     cat['path'] ='/Users/USER/AGNfitter/'  #path to the AGNfitter code
-
-
     cat['filename'] = cat['path']+'data/catalog_example.txt'
     cat['filetype'] = 'ASCII' ## catalog file type: 'ASCII' or 'FITS'. 
-                              ## FITS option not available yet. 
+    cat['name'] = 0                 ## If ASCII: Column index (int) of source IDs
+                                    ## If FITS : Column name (str). E.g. 'ID'
+    cat['redshift'] = 1             ## If ASCII:  Column index(int) of redshift 
+                                     ## If FITS : Column name (str). E.g. z'
 
-    cat['name'] = 0#'ID'            ## If ASCII: Column index (int) of source IDs
-                                        ## If FITS: not yet
-    cat['redshift'] = 1#'z'              ## If ASCII:  Column index(int) of redshift
-                                        ## If FITS: not yet
- 
     ##FREQUENCIES/WAVELENGTHS 
     ## if ASCII specify 'freq/wl_list', if FITS specify 'freq/wl_suffix'
     cat['freq/wl_list'] = np.arange(2,56,3).tolist()                                  
@@ -47,15 +43,14 @@ def CATALOG_settings():
     #cat['freq/wl_suffix'] = '_wl'      ## If FITS: common ending to wavelength column names
     cat['freq/wl_format'] = 'wavelength' ## Gives your catalog *observed*
                                          ## 'frequency' or 'wavelength'?
-    cat['freq/wl_unit'] = u.Angstrom       ## Astropy unit of freq or wavelength
+    cat['freq/wl_unit'] = u.Angstrom     ## Astropy unit of freq or wavelength
 
     ##FLUXES 
     ## if ASCII specify 'freq/wl_list', if FITS specify 'freq/wl_suffix'
     cat['flux_unit'] = u.Jy             ## Astropy unit of *flux* (astropy-units)
     cat['flux_list'] = np.arange(3,57,3).tolist()        
                                         ## If ASCII: List of column indexes (int)
-    #cat['flux_suffix'] = '_f'          ## If FITS: Common ending of all flux column names (str)
-    
+    #cat['flux_suffix'] = '_f'          ## If FITS: Common ending of all flux column names (str)    
     cat['fluxerr_list'] = np.arange(4,58,3).tolist() 
                                         ## If ASCII: List of column indexes (int)
     #cat['fluxerr_suffix'] = '_e'       ## If FITS: common ending to fluxerr column names (str)
@@ -77,7 +72,6 @@ def CATALOG_settings():
                                       # Default: cat['path'] (same as AGNfitter code path) 
 
     cat['output_folder'] =  cat['workingpath'] +'OUTPUT/' #if no special OUTPUT folder, leave default
-    cat['dict_path'] = cat['workingpath'] + 'models/MODELSDICT_default' 
 
 
     return cat
@@ -93,63 +87,93 @@ def FILTERS_settings():
     filters = dict()
 
     filters['dict_zarray'] =np.array([0.283, 1.58])  # The grid of redshifts needed to fit your catalog
-    filters['Bandset'] = 'BANDSET_default' # OPTIONS: 
-                                           # 'BANDSET_default' (for testing)
-                                           # 'BANDSET_settings' (choosing relevant filters below, as given by your catalog)
-                                           # if your filter is not included, go to DICTIONARIES_AGNfitter to add.
+    filters['path'] = 'models/FILTERS/' 
+    filters['filterset'] = 'filterset_default' ## 'filterset_default' (for the test case),
+                                               ## for the user's case: customize, eg. filtersv1
+    
+    filters['SPIRE500'] = True
+    filters['SPIRE350'] = True
+    filters['SPIRE250'] = True
+    filters['PACS160'] = True
+    filters['PACS100'] = True
+    filters['MIPS160'] = True
+    filters['MIPS70'] = True
+    filters['MIPS24'] = True
+    filters['IRAC4'] = True
+    filters['IRAC3'] = True
+    filters['IRAC2'] = True
+    filters['IRAC1'] = True
+    filters['WISE4'] = True
+    filters['WISE3'] = True
+    filters['WISE2'] = True
+    filters['WISE1'] = True
+    filters['Ks_2mass'] = True
+    filters['H_2mass'] = True
+    filters['J_2mass'] = True
+    filters['H_VISTA'] = True
+    filters['J_VISTA'] = True
+    filters['K_VISTA'] = True
+    filters['Y_VISTA'] = True
+    filters['u_CHFT'] = True
+    filters['g_CHFT'] = True
+    filters['r_CHFT'] = True
+    filters['i_CHFT'] = True
+    filters['z_CHFT'] = True
+    filters['u_SDSS'] = True
+    filters['g_SDSS'] = True
+    filters['r_SDSS'] = True
+    filters['i_SDSS'] = True
+    filters['z_SDSS'] = True
+    filters['g_SUBARU'] = True
+    filters['r_SUBARU'] = True
+    filters['i_SUBARU'] = True
+    filters['z_SUBARU'] = True
+    filters['B_SUBARU'] = True
+    filters['V_SUBARU'] = True
+    filters['GALEX_2500'] = True
+    filters['GALEX_1500'] = True
+    filters['MUSYC_U'] = True
+    filters['MUSYC_B'] = True
+    filters['MUSYC_V'] = True
+    filters['MUSYC_R'] = True
+    filters['MUSYC_I'] = True
+    filters['MUSYC_z'] = True
 
-    filters['SPIRE500']= True
-    filters['SPIRE350']= True
-    filters['SPIRE250']= True
-    filters['PACS160']=False
-    filters['PACS100']=False
+    filters['add_filters']= False # If 'True' please add them below in ADD FILTERS
 
-    filters['MIPS160']=False      
-    filters['MIPS70']=False    
-    filters['MIPS24']=True
+    """==================================
+    ADD FILTERS (optional)
+    =================================="""
 
-    filters['IRAC4']=True       
-    filters['IRAC3']=True
-    filters['IRAC2']=True
-    filters['IRAC1']=True
+    ADDfilters=dict()
+    ADDfilters['names'] = []    ## (string/list of strings)User especified filter names. 
+                                ## If name has been previously used, an error message will appear. 
+    ADDfilters['filenames'] =[] ## (string/list of strings) File names of the new filters. 
+                                ## File format: 2 columns of 1) freq/wavelength 2) Throughput. 
+                                ## Path assumed is the cat['path'] especified above. 
+                                ## Example: 'models/FILTERS/my_new_filter.txt'
+    ADDfilters['freq/wl_format'] = ['wavelength'] * len(ADDfilters['names']) ## Info about the column 1 of your filter file.
+                                                                             ## Options: 'wavelength' or 'frequency'.    
+    ADDfilters['freq/wl_unit'] =  [u.Angstrom]* len(ADDfilters['names']) ## (Astropy Unit) Info about the column 1 
+                                                                         ## of your filter file. 
+    ADDfilters['description'] = ['description_dummy']* len(ADDfilters['names']) ## (Str) Any description the user wants to give 
+                                                                                ##  to the filter to add.
 
-    filters['WISE4']=False
-    filters['WISE3']=False
-    filters['WISE2']=False
-    filters['WISE1']=False
-
-    filters['Ks_2mass']=True
-    filters['H_2mass']=True
-    filters['J_2mass']=True
-
-    filters['H_VISTA']=False
-    filters['J_VISTA']=False
-    filters['K_VISTA']=False
-    filters['Y_VISTA']=True
-
-    filters['u_SDSS']=False  
-    filters['g_SDSS']=False
-    filters['r_SDSS']=False
-    filters['i_SDSS']=False  
-    filters['z_SDSS']=False
-
-    filters['g_SUBARU']=False
-    filters['r_SUBARU']=True
-    filters['i_SUBARU']=True  
-    filters['z_SUBARU']=True
-    filters['B_SUBARU']=True
-    filters['V_SUBARU']=False
-
-    filters['u_CHFT']=True  
-    filters['g_CHFT']=False
-    filters['r_CHFT']=False
-    filters['i_CHFT']=False  
-    filters['z_CHFT']=False
-
-    filters['GALEX_2500']=True
-    filters['GALEX_1500']=False
+    filters['add_filters_dict']= ADDfilters
 
     return filters
+
+def MODELS_settings():
+
+    """==================================
+    Work in progress
+    =================================="""
+
+    models = dict()
+    models['path'] = 'models/' 
+    models['modelset'] = 'modelsv1'
+
+    return models
 
 def MCMC_settings():
 
